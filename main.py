a73408372fe6d49c8d878f1b4c6c2f66e3f7e2d9
Rishabh singh <rishabhhsingh0@gmail.com>
2024-04-20 23:26:09 +0530

    ID

--- main.py
+++ main.py
@@ -1,52 +1,6 @@
 #main
-#Project 9.
-##program to show-How to merge two dictionaries
-##creating two separate dictionaries namely,'boys','girls'.
-boys={'Nilesh':41,'Soumitra':42,'Nadeem':47}
-girls={'Rasika':38,'Rajashree':43,'Rasika':45}
-
-## Now combining
-combined={**boys,**girls}
-print(combined)
-combined={**girls,**boys}
-print(combined)
-
-
-
-
-#Project2
-## Writing code to get required results on the string "Bamboozled".
-s='Bamboozled'
-
-# extract B a
-print(s[0], s[1])
-print(s[-10], s[-9])
-
-#extract e d
-print(s[8], s[9])
-print(s[-2], s[-1])
-
-#extract mboozled
-print(s[2:10])
-print(s[2:])
-print(s[-8:])
-
-#extract Bamboo
-print(s[0:6])
-print(s[:6])
-print(s[-10:-4])
-print(s[:-4])
-
-#reverse Bamboozled
-print(s[::-1])
-
-
-print(s[0:10:1])
-print(s[0:10:2])
-print(s[0:10:3])
-print(s[0:10:4])
-
-s= s+'Hype!'
-print(s)
-s=s[:6] + 'Monger' +s[-1]
-print(s)
+print("The creator is a student.")
+print("The name's RISHABH SINGH.")
+print("235/UCD/046")
+print("1st year,CSE, Data Science.")
+print("SEM II-2024,2023-2027")
